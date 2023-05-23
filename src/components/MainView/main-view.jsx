63ea62a0078a4f6f23f6138547c5138c1f16d157
urenami <122377715+urenami@users.noreply.github.com>
2023-05-22 22:49:06 -0400

    updated main-view

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -9,44 +9,24 @@ export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-<<<<<<< Updated upstream
     fetch("https://my-flixdb-56034.herokuapp.com")
       .then((response) => response.json())
-      .then((data) => {
-        const moviesFromApi = data.docs.map((doc) => {
-          return {
-            id: doc.key,
-            title: doc.title,
-            Director: {
-              firstName: movie.Director.firstName,
-              lastName: movie.Director.lastName,
-            },
-          };
-        });
-
-        setMovies(moviesFromApi);
-      })
-      .catch((error) => {
-        console.log("Error fetching movies:", error);
-=======
-    fetch("https://my-flixdb-56034.herokuapp.com/movies")
-      .then((res) => res.json())
       .then((data) => {
         const moviesFromApi= data.map((movie) => {
           return {
             _id: movie._id,
             Title: movie.Title,
             Description: movie.Description,
+            Release_date: movie.Release_date,
             Genre: movie.Genre,
             Director: movie.Director,
-            imageURL: movie.ImageURL,
+            ImageURL: movie.ImageURL,
             Actors: movie.Actors
           }
         }) 
 
         setMovies(moviesFromApi);
         console.log(moviesFromApi)
->>>>>>> Stashed changes
       });
   }, []);
 
