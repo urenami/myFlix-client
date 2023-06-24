cbca6ee8b1a1b75ddba4bb65ac058b70f4162397
urenami <122377715+urenami@users.noreply.github.com>
2023-06-23 21:43:57 -0400

    Revert "changes to index.jsx"
    This reverts commit cd2ffdf34babea3d5f4eb91c50591883a432db3a.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/MainView/main-view";
 import Container from "react-bootstrap/Container";
-import { store } from "./redux/store";
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
