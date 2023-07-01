5645b1748ac1501aef1871223ff222d2ecb1e742
urenami <122377715+urenami@users.noreply.github.com>
2023-07-01 16:55:35 -0400

    remove  user reducer

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './reducers/user';
 
 export const store = configureStore({
   reducer: { movies: moviesReducer, user: userReducer },
