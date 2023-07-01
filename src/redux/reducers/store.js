026e2c0fcccd819af780d895b5ad5046fdd5ae96
urenami <122377715+urenami@users.noreply.github.com>
2023-07-01 17:00:53 -0400

    Revert "updated store.js"
    This reverts commit 4690adc208c39b576e6d3bfdbe54ff235d7bae41.

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import moviesReducer from "./reducers/movies";
+import userReducer from "./reducers/user";
 
 export const store = configureStore({
   reducer: { movies: moviesReducer, user: userReducer },
