4690adc208c39b576e6d3bfdbe54ff235d7bae41
urenami <122377715+urenami@users.noreply.github.com>
2023-07-01 16:42:01 -0400

    updated store.js

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,6 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import moviesReducer from "./reducers/movies";
-import userReducer from "./reducers/user";
 
 export const store = configureStore({
   reducer: { movies: moviesReducer, user: userReducer },
