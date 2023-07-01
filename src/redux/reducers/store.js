2fbce16992cf57a7dc4ae9e3ec1b3a72848dc40a
urenami <122377715+urenami@users.noreply.github.com>
2023-07-01 17:00:23 -0400

    Revert "remove  user reducer"
    This reverts commit 5645b1748ac1501aef1871223ff222d2ecb1e742.

--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from './reducers/user';
 
 export const store = configureStore({
   reducer: { movies: moviesReducer, user: userReducer },
