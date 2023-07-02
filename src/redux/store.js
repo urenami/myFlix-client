e6ec40e2acd26ecfe446077c5f16b70972181f03
urenami <122377715+urenami@users.noreply.github.com>
2023-07-02 19:50:13 -0400

    error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import moviesReducer from './reducers/movies';
-import userReducer from "./reducers/user";
+import userReducer from './reducers/user';
 
 export const store = configureStore({
   reducer: { books: moviesReducer, user: userReducer }
