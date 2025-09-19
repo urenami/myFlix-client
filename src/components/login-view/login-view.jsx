6c0416c40bad3bcc35bda647fffbf270b798bf3a
urenami <122377715+urenami@users.noreply.github.com>
2025-09-18 22:47:22 -0400

    Small fixes in login view (error handling/UI)

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,66 +3,65 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-
-export const LoginView = ({onLoggedIn}) => {
+export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const handleSubmit = (event) => {
     //this prevents the default behaviour of the form which is to reload the entire page
     event.preventDefault();
 
-  const data = {
-    Username: username,
-    Password: password
+    const data = {
+      Username: username,
+      Password: password,
+    };
+
+    fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went worng. Please try again.");
+      });
   };
 
-  fetch ("https://my-flixdb-56034.herokuapp.com/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json"},
-    body: JSON.stringify(data)
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Login response: ", data);
-    if (data.user) {
-      localStorage.setItem("user", JSON.stringify(data.user));
-      localStorage.setItem("token", data.token);
-      onLoggedIn(data.user, data.token);
-    } else {
-      alert("No such user");
-    }
-  })
-  .catch((e) => {
-    alert("Something went worng. Please try again.");
-  });
-}
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="3"
+        />
+      </Form.Group>
 
-return (
-  <Form onSubmit={handleSubmit}>
-    <Form.Group controlId="formUsername">
-      <Form.Label>Username:</Form.Label>
-      <Form.Control
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        required
-        minLength="3"
-      />
-    </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </Form.Group>
 
-    <Form.Group controlId="formPassword">
-      <Form.Label>Password:</Form.Label>
-      <Form.Control
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-    </Form.Group>
-
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
